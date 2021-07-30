5368cfe1a57ef19b58d4a4f2d25187e7726f0a1f
Carl Leitner <litlfred@gmail.com>
2021-07-30 15:46:28 -0400

    added script to process .map into .json files

---
+++ create-maps.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+### @host = http://localhost:8080/hapi-fhir-jpavalidator/fhir
+### @host = http://test.ahdis.ch/hapi-fhir-jpavalidator/fhir
+### @host = https://ehealthsuisse.ihe-europe.net/hapi-fhir-jpavalidator/fhir
+
+
+#HOST="http://127.0.0.1:8080/r4"
+#HOST="http://hapi.fhir.org/baseR4"
+HOST="http://test.ahdis.ch/r4"
+
+echo "Accessing $HOST/metadata"
+
+### Get capability statement
+#curl --request GET $HOST/metadata \
+#     -H "Accept: application/fhir+json" 
+
+
+DIR="input/resources/maps/*.map"
+for FILE in ls $DIR
+do    
+    echo "Updating DDCC to Core Data Set Structure Map $FILE"
+    curl  --request POST $HOST/StructureMap \
+      --data-binary @$FILE \
+      -H "Accept: application/fhir+json"  -H "Content-Type: text/fhir-mapping" \
+      > $FILE.json
+done
