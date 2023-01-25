9f8354b77be45c389bb579bf40eab48e076ac90c
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2023-01-25 13:18:10 -0500

    change karate jar filename

--- structuremap-tests/structuremap-tests.sh
+++ structuremap-tests/structuremap-tests.sh
@@ -2,15 +2,14 @@
 set -e
 
 # download karate standalone jar
-if ! [ -f ./karate-1.3.1.jar ]; then
-  curl -L https://github.com/karatelabs/karate/releases/download/v1.3.1/karate-1.3.1.jar --output karate-1.3.1.jar
+if ! [ -f ./karate.jar ]; then
+  curl -L https://github.com/karatelabs/karate/releases/download/v1.3.1/karate-1.3.1.jar --output karate.jar
 fi
 
-# run sushi
+# run all tests against Matchbox server
+# requires Sushi to be run first
 sushi ..
-
-# run all tests
-java -jar karate-1.3.1.jar structuremap-tests.feature
+java -jar karate.jar -t @matchbox structuremap-tests.feature
 
 # optionally run specific tests by tag
-#java -jar karate-1.3.1.jar -t @shc structuremap-tests.feature
+#java -jar karate.jar -t @matchbox -t @shc structuremap-tests.feature
