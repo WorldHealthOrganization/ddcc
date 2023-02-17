cf451aa7021b5768a3a057554853fe706344e1a7
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2023-02-17 17:39:27 -0500

    revert local changes

--- structuremap-tests/structuremap-tests.sh
+++ structuremap-tests/structuremap-tests.sh
@@ -9,8 +9,7 @@ fi
 # run all tests against Matchbox server
 # requires Sushi to be run first
 sushi ..
-# java -jar karate.jar -t @matchbox structuremap-tests.feature
-# ^matchbox docker cmd^ docker run -d -p 8080:8080 mitre
+java -jar karate.jar -t @matchbox structuremap-tests.feature
 
 # optionally run specific tests by tag
 #java -jar karate.jar -t @matchbox -t @shc structuremap-tests.feature
@@ -18,5 +17,4 @@ sushi ..
 # run tests against local copy of validator_cli.jar instead of Matchbox server
 # requires IG publisher to be run first
 # update jar classpath to path of local validator_cli.jar file, see https://confluence.hl7.org/display/FHIR/Using+the+FHIR+Validator
-java -Xmx8192m -cp karate.jar:/Users/rscalfani/Documents/code/validator_cli.jar com.intuit.karate.Main -t @validator structuremap-tests.feature
-# ^IG Publisher Command^ ~ java -jar publisher.jar -ig dvcc/ddcc/ig.ini
+#java -cp karate.jar:validator_cli.jar com.intuit.karate.Main -t @validator structuremap-tests.feature
